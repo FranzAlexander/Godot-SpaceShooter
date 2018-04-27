bf24aac56386eae6cf22a216edd64eacaeef5a32
AlexanderFranz <alexbalcombe4@gmail.com>
2018-04-27 10:59:40 +0800

    Movement
    - I have added basic movement to the character/player.
    - Not 100% sure on why up and down was inverted but fixed now.

--- SpaceShooter/Scripts/Player.gd
+++ SpaceShooter/Scripts/Player.gd
@@ -1,7 +1,29 @@
 extends KinematicBody2D
 
+export (int) var moveSpeed = 10
+
+var velocity = Vector2()
 var movement = Vector2()
 
+#Get input from the arrow keys then applies the movement. Arrow keys where mapped using the built-in Input Map feature.
+func get_input():
+	
+	velocity = Vector2()
+	
+	if Input.is_action_pressed('ui_right'):
+		velocity.x += 1
+	if Input.is_action_pressed('ui_left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('ui_up'):
+		velocity.y -= 1
+	if Input.is_action_pressed('ui_down'):
+		velocity.y += 1
+	velocity = velocity.normalized() * moveSpeed
+	
+	pass
+
+
 func _physics_process(delta):
-	move_and_collide(Vector2(movement.x,0))
+	get_input()
+	move_and_collide(velocity)
 	pass
